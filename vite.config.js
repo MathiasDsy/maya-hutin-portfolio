2704528a6aaf66f3f4783bfec82076c5ec25a438
Mathias Desoyer <mathias.desoyer29@gmail.com>
2025-11-02 21:23:47 +0100

    deploy hotfix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/maya-hutin-portfolio/', // 👈 c'est cette ligne qui est cruciale
 })
